1d6dba387dde114d30fadcd54940bc1ff421cf81
Mohamed Ouhami <mohamed.ouhami2001@gmail.com>
2023-10-25 20:52:03 +0100

    first commit

---
+++ localisation/src/main/java/ma/ouhami/localisation/position/Position.java
@@ -0,0 +1,28 @@
+package ma.ouhami.localisation.position;
+
+
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Position {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private double longitude;
+    private double latitude;
+
+    private LocalDateTime datetime;
+    private String imei;
+
+}
---
+++ localisation/src/main/java/ma/ouhami/localisation/position/PositionRepository.java
@@ -0,0 +1,7 @@
+package ma.ouhami.localisation.position;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PositionRepository extends JpaRepository<Position,Integer>{
+    
+}
